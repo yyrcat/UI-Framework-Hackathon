1a6b84ec3a0deddeb23c30f07b7ebef645e2304f
justinbrowne <justinbrowne@live.com>
2017-02-25 17:55:20 -0800

    Restored master.

---
+++ scripts/markupDropDown.js
@@ -0,0 +1,24 @@
+var markupBtn = document.querySelectorAll('.btn-full-width');
+
+var forEach = function(array, callback, scope) {
+  for (var i = 0; i < array.length; i++) {
+    callback.call(scope, i, array[i]);
+  }
+};
+
+forEach(markupBtn,function(index, elem) {
+  elem.addEventListener('click', function(ev) {
+    ev.preventDefault();
+    var showMarkup = this.nextElementSibling;
+
+    showMarkup.classList.toggle('slide-down');
+    this.classList.toggle('btn-bottom-no-radius');
+
+    if(this.classList.contains('btn-bottom-no-radius')) {
+      this.innerHTML = "Hide markup";
+    } else {
+      this.innerHTML = "Show markup";
+    }
+  }, false);
+
+});
